e29b417ca6ec37173dbd620c9a9717b098b032e2
Dogsky <TaichiSky@users.noreply.github.com>
2020-11-23 12:45:45 +0800

    Update UserController.java
    Use @RestController annotation instead of @Controller and @ResponseBody annotations

--- src/main/java/com/example/shop/controller/UserController.java
+++ src/main/java/com/example/shop/controller/UserController.java
@@ -5,14 +5,12 @@ import com.example.shop.dto.User;
 import com.example.shop.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/user")
 public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
